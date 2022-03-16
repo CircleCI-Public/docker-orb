500a589c18fc616e1665451ba66ceabe16610a47
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-03-16 16:54:01 -0700

    fix shellcheck errors

--- src/scripts/configure-docker-credentials-store.sh
+++ src/scripts/configure-docker-credentials-store.sh
@@ -2,8 +2,8 @@
 
 HELPER_NAME="$PARAM_HELPER_NAME"
 
-if [ -z "${PARAM_HELPER_NAME}" ]; then
-  if [ -n "$(uname | grep "Darwin")" ]; then
+if [ -z "${HELPER_NAME}" ]; then
+  if uname | grep -q "Darwin"; then
     HELPER_NAME="osxkeychain"
   else
     HELPER_NAME="pass"
@@ -12,12 +12,12 @@ fi
 
 if [ ! -f "$PARAM_DOCKER_CONFIG_PATH" ]; then
   echo "${PARAM_DOCKER_CONFIG_PATH} does not exist; initializing it..."
-  mkdir -p $(dirname "$PARAM_DOCKER_CONFIG_PATH")
+  mkdir -p "$(dirname "$PARAM_DOCKER_CONFIG_PATH")"
   echo "{}" > "$PARAM_DOCKER_CONFIG_PATH"
 fi
 
 cat "$PARAM_DOCKER_CONFIG_PATH" |
   jq --arg credsStore "$HELPER_NAME" '. + {credsStore: $credsStore}' \
     >/tmp/docker-config-credsstore-update.json
-cat /tmp/docker-config-credsstore-update.json >"$PARAM_DOCKER_CONFIG_PATH"
+cat /tmp/docker-config-credsstore-update.json > "$PARAM_DOCKER_CONFIG_PATH"
 rm /tmp/docker-config-credsstore-update.json

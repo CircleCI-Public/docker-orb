faf75f6395710e9c2f51df3da50c7ce249ec11a2
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-03-17 14:13:10 -0700

    comment npm install to debug

--- src/scripts/dockerlint.sh
+++ src/scripts/dockerlint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+# if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
-if [ "$PARAM_DEBUG" = true ]; then
-  npm install -g dockerlint || "$SUDO" npm install -g dockerlint
-else
-  npm install -g dockerlint > /dev/null 2>&1 || "$SUDO" npm install -g dockerlint > /dev/null 2>&1
-fi
+# if [ "$PARAM_DEBUG" = true ]; then
+#   npm install -g dockerlint || "$SUDO" npm install -g dockerlint
+# else
+#   npm install -g dockerlint > /dev/null 2>&1 || "$SUDO" npm install -g dockerlint > /dev/null 2>&1
+# fi
 
 if [ "$PARAM_TREAT_WARNING_AS_ERRORS" = true ]; then
   dockerlint -p "$PARAM_DOCKERFILE"

cdcd4f62653779220dbaabe643bffcac277830a7
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-03-17 15:18:12 -0700

    change back to original dockerlint config

--- src/scripts/dockerlint.sh
+++ src/scripts/dockerlint.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-# if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
-# if [ "$PARAM_DEBUG" = true ]; then
-#   npm install -g dockerlint || "$SUDO" npm install -g dockerlint
-# else
-#   npm install -g dockerlint > /dev/null 2>&1 || "$SUDO" npm install -g dockerlint > /dev/null 2>&1
-# fi
+if [ "$PARAM_DEBUG" = true ]; then
+  npm install -g dockerlint || "$SUDO" npm install -g dockerlint
+else
+  npm install -g dockerlint > /dev/null 2>&1 || "$SUDO" npm install -g dockerlint > /dev/null 2>&1
+fi
 
-# if [ "$PARAM_TREAT_WARNING_AS_ERRORS" = true ]; then
-#   dockerlint -p "$PARAM_DOCKERFILE"
-# else
-#   dockerlint "$PARAM_DOCKERFILE"
-# fi
-
-dockerlint -p "$PARAM_DOCKERFILE"
+if [ "$PARAM_TREAT_WARNING_AS_ERRORS" = true ]; then
+  dockerlint -f "$PARAM_DOCKERFILE" -p
+else
+  dockerlint -f "$PARAM_DOCKERFILE"
+fi

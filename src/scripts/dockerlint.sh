7199f7b9b510fd34de0a979a0eb1b2dc9adb077c
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-03-18 12:01:15 -0700

    replace dockerlint test and example executors
    I am adding a cimg/node image in the dockerlint integrations tests.
    Without npm dockerlint can't be installed and npm isn't available in
    cimg/base, hence the need for cimg/node.
    The example will also be updated.

--- src/scripts/dockerlint.sh
+++ src/scripts/dockerlint.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+if [[ $EUID == 0 ]]; then SUDO=""; else SUDO="sudo"; fi
 
-if [ "$PARAM_DEBUG" = true ]; then
-  npm install -g dockerlint || "$SUDO" npm install -g dockerlint
-else
-  npm install -g dockerlint > /dev/null 2>&1 || "$SUDO" npm install -g dockerlint > /dev/null 2>&1
+if ! command -v dockerlint 1> /dev/null 2> /dev/null; then
+  if ! command -v npm 1> /dev/null 2> /dev/null; then 
+    echo "npm is required to install dockerlint.";
+    echo "Consider running this command with an image that has node available: https://circleci.com/developer/images/image/cimg/node"; 
+    echo "Alternatively, use dockerlint's docker image: https://github.com/RedCoolBeans/dockerlint#docker-image."
+    exit 1
+  fi
+
+  if [ "$PARAM_DEBUG" = true ]; then
+    npm install -g dockerlint || "$SUDO" npm install -g dockerlint
+  else
+    npm install -g dockerlint 1> /dev/null 2> /dev/null || "$SUDO" npm install -g dockerlint 1> /dev/null 2> /dev/null
+  fi
 fi
 
 if [ "$PARAM_TREAT_WARNING_AS_ERRORS" = true ]; then

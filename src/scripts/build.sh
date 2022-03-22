da7d820ecff085df97c72dadf5456287a06feb5f
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-03-22 15:12:52 -0700

    replacing quotes with dollar sign to check for fix

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -14,7 +14,7 @@ if [ -z "$PARAM_CACHE_FROM" ]; then
     COMMAND="docker build -f ${PARAM_DOCKERFILE_PATH}/${PARAM_DOCKERFILE_NAME} ${docker_tag_args} ${PARAM_DOCKER_CONTEXT}"
     echo "Running: ${COMMAND}"
     
-    docker build -f "$PARAM_DOCKERFILE_PATH"/"$PARAM_DOCKERFILE_NAME" "$docker_tag_args" "$PARAM_DOCKER_CONTEXT"
+    docker build -f "$PARAM_DOCKERFILE_PATH"/"$PARAM_DOCKERFILE_NAME" ${docker_tag_args} "$PARAM_DOCKER_CONTEXT"
   else
     echo "I'm the else for extra args"
     # COMMAND="docker build ${PARAM_EXTRA_BUILD_ARGS} -f ${PARAM_DOCKERFILE_PATH}/${PARAM_DOCKERFILE_NAME} ${docker_tag_args} ${PARAM_DOCKER_CONTEXT}"

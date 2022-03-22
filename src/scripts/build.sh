676a637f465c88500c5c96030924f7c61f9d0fa5
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-03-22 15:07:57 -0700

    quoting params again to force error

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -14,30 +14,30 @@ if [ -z "$PARAM_CACHE_FROM" ]; then
     COMMAND="docker build -f ${PARAM_DOCKERFILE_PATH}/${PARAM_DOCKERFILE_NAME} ${docker_tag_args} ${PARAM_DOCKER_CONTEXT}"
     echo "Running: ${COMMAND}"
     
-    docker build -f ${PARAM_DOCKERFILE_PATH}/${PARAM_DOCKERFILE_NAME} ${docker_tag_args} ${PARAM_DOCKER_CONTEXT}
+    docker build -f "$PARAM_DOCKERFILE_PATH"/"$PARAM_DOCKERFILE_NAME" "$docker_tag_args" "$PARAM_DOCKER_CONTEXT"
   else 
-    COMMAND="docker build ${PARAM_EXTRA_BUILD_ARGS} -f ${PARAM_DOCKERFILE_PATH}/${PARAM_DOCKERFILE_NAME} ${docker_tag_args} ${PARAM_DOCKER_CONTEXT}"
-    echo "Running: ${COMMAND}"
+    # COMMAND="docker build ${PARAM_EXTRA_BUILD_ARGS} -f ${PARAM_DOCKERFILE_PATH}/${PARAM_DOCKERFILE_NAME} ${docker_tag_args} ${PARAM_DOCKER_CONTEXT}"
+    # echo "Running: ${COMMAND}"
 
-    docker build "$PARAM_EXTRA_BUILD_ARGS" -f "$PARAM_DOCKERFILE_PATH"/"$PARAM_DOCKERFILE_NAME" "$docker_tag_args" "$PARAM_DOCKER_CONTEXT"
+    # docker build "$PARAM_EXTRA_BUILD_ARGS" -f "$PARAM_DOCKERFILE_PATH"/"$PARAM_DOCKERFILE_NAME" "$docker_tag_args" "$PARAM_DOCKER_CONTEXT"
   fi
 
 else
-  echo "$PARAM_CACHE_FROM" | sed -n 1'p' | tr ',' '\n' | while read -r image; do
-    echo "Pulling ${image}";
-    docker pull ${image} || true
-  done
+  # echo "$PARAM_CACHE_FROM" | sed -n 1'p' | tr ',' '\n' | while read -r image; do
+  #   echo "Pulling ${image}";
+  #   docker pull ${image} || true
+  # done
 
-  docker_tag_args=""
+  # docker_tag_args=""
 
-  IFS="," read -ra DOCKER_TAGS <<< "$PARAM_TAG"
+  # IFS="," read -ra DOCKER_TAGS <<< "$PARAM_TAG"
 
-  for tag in "${DOCKER_TAGS[@]}"; do
-    docker_tag_args="${docker_tag_args} -t ${PARAM_REGISTRY}/${PARAM_IMAGE_NAME}:${tag}"
-  done
+  # for tag in "${DOCKER_TAGS[@]}"; do
+  #   docker_tag_args="${docker_tag_args} -t ${PARAM_REGISTRY}/${PARAM_IMAGE_NAME}:${tag}"
+  # done
 
-  docker build "$PARAM_EXTRA_BUILD_ARGS" "$PARAM_EXTRA_BUILD_ARGS" --cache-from "$PARAM_CACHE_FROM" \
-    -f "$PARAM_DOCKERFILE_PATH"/"$PARAM_DOCKERFILE_NAME" \
-    $docker_tag_args \
-    "$PARAM_DOCKER_CONTEXT"
+  # docker build "$PARAM_EXTRA_BUILD_ARGS" "$PARAM_EXTRA_BUILD_ARGS" --cache-from "$PARAM_CACHE_FROM" \
+  #   -f "$PARAM_DOCKERFILE_PATH"/"$PARAM_DOCKERFILE_NAME" \
+  #   $docker_tag_args \
+  #   "$PARAM_DOCKER_CONTEXT"
 fi

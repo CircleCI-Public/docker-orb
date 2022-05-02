e5923ed8f5c51845607acc231e89c17db1254998
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-05-02 12:09:46 -0700

    refactor: extract check and credentials

---
+++ src/scripts/configure-docker-credentials-store.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+HELPER_NAME="$PARAM_HELPER_NAME"
+DOCKER_CONFIG_PATH="$(eval echo ${PARAM_DOCKER_CONFIG_PATH})"
+
+if [ -z "${HELPER_NAME}" ]; then
+  if uname | grep -q "Darwin"; then
+    HELPER_NAME="osxkeychain"
+  else
+    HELPER_NAME="pass"
+  fi
+fi
+
+if [ ! -e "$DOCKER_CONFIG_PATH" ]; then
+  echo "${DOCKER_CONFIG_PATH} does not exist; initializing it..."
+  mkdir -p "$(dirname "$DOCKER_CONFIG_PATH")"
+  echo "{}" > "$DOCKER_CONFIG_PATH"
+fi
+
+cat "$DOCKER_CONFIG_PATH" |
+  jq --arg credsStore "$HELPER_NAME" '. + {credsStore: $credsStore}' \
+    >/tmp/docker-config-credsstore-update.json
+cat /tmp/docker-config-credsstore-update.json > "$DOCKER_CONFIG_PATH"
+
+rm /tmp/docker-config-credsstore-update.json

4b53b6a75c2ff9d94e64d981908116cdaa380b5f
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-06-13 09:33:30 -0700

    fix: evaluate tag when digest path is set

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -10,5 +10,6 @@ done
 if [ -n "$PARAM_DIGEST_PATH" ]; then
   mkdir -p "$(dirname "$PARAM_DIGEST_PATH")"
   IFS="," read -ra DOCKER_TAGS <<< "$PARAM_TAG"
-  docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY"/"$PARAM_IMAGE":"${DOCKER_TAGS[0]}" > "$PARAM_DIGEST_PATH"
+  tag=$(eval echo "${DOCKER_TAGS[0]}")
+  docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY"/"$PARAM_IMAGE":"$tag" > "$PARAM_DIGEST_PATH"
 fi

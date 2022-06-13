e7143bfa5c13d8323a2e83323fc9e3ddbc352d61
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-06-13 13:47:42 -0300

    fix: import the `push.sh` script in the push command (#131)
    * fix: import push script in the command
    
    * fix: evaluate tag when digest path is set

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -2,12 +2,14 @@
 
 IFS="," read -ra DOCKER_TAGS <<< "$PARAM_TAG"
 
-for tag in "${DOCKER_TAGS[@]}"; do
-  docker push "$PARAM_REGISTRY"/"$PARAM_IMAGE":${tag}
+for docker_tag in "${DOCKER_TAGS[@]}"; do
+  tag=$(eval echo "$docker_tag")
+  docker push "$PARAM_REGISTRY"/"$PARAM_IMAGE":"$tag"
 done
 
 if [ -n "$PARAM_DIGEST_PATH" ]; then
   mkdir -p "$(dirname "$PARAM_DIGEST_PATH")"
   IFS="," read -ra DOCKER_TAGS <<< "$PARAM_TAG"
-  docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY"/"$PARAM_IMAGE":"${DOCKER_TAGS[0]}" > "$PARAM_DIGEST_PATH"
+  tag=$(eval echo "${DOCKER_TAGS[0]}")
+  docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY"/"$PARAM_IMAGE":"$tag" > "$PARAM_DIGEST_PATH"
 fi

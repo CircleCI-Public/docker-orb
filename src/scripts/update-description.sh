0a6b0c50eb78421b6834e8853ab054e901032194
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-08-19 12:12:03 -0700

    fix: support for env vars param in `publish`

--- src/scripts/update-description.sh
+++ src/scripts/update-description.sh
@@ -7,12 +7,13 @@ fi
 
 USERNAME=${!PARAM_DOCKER_USERNAME}
 PASSWORD=${!PARAM_DOCKER_PASSWORD}
+IMAGE="$(eval echo "$PARAM_IMAGE")"
 
 DESCRIPTION="$PARAM_PATH/$PARAM_README"
 PAYLOAD="username=$USERNAME&password=$PASSWORD"
 JWT=$(curl -s -d "$PAYLOAD" https://hub.docker.com/v2/users/login/ | jq -r .token)
 HEADER="Authorization: JWT $JWT"
-URL="https://hub.docker.com/v2/repositories/$PARAM_IMAGE/"
+URL="https://hub.docker.com/v2/repositories/$IMAGE/"
 STATUS=$(curl -s -o /dev/null -w '%{http_code}' -X PATCH -H "$HEADER" --data-urlencode full_description@$DESCRIPTION $URL)
 
 if [ $STATUS -ne 200 ]; then

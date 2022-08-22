422ce691ce032d3d114236142f219e6623760e7b
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-08-22 16:56:19 -0300

    fix: support for env vars parameters in `publish` (#142)
    * test: include tests for new use use
    
    * test: fix macos version due to deprecation
    
    * fix: support for env vars param in `publish`

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -2,14 +2,19 @@
 
 IFS="," read -ra DOCKER_TAGS <<< "$PARAM_TAG"
 
+image="$(eval echo "$PARAM_IMAGE")"
+
 for docker_tag in "${DOCKER_TAGS[@]}"; do
   tag=$(eval echo "$docker_tag")
-  docker push "$PARAM_REGISTRY"/"$PARAM_IMAGE":"$tag"
+
+  set -x
+  docker push "$PARAM_REGISTRY"/"$image":"$tag"
+  set +x
 done
 
 if [ -n "$PARAM_DIGEST_PATH" ]; then
   mkdir -p "$(dirname "$PARAM_DIGEST_PATH")"
   IFS="," read -ra DOCKER_TAGS <<< "$PARAM_TAG"
   tag=$(eval echo "${DOCKER_TAGS[0]}")
-  docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY"/"$PARAM_IMAGE":"$tag" > "$PARAM_DIGEST_PATH"
+  docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY"/"$image":"$tag" > "$PARAM_DIGEST_PATH"
 fi

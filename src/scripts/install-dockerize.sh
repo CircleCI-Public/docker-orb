e991aee15d84a84d4ffcecc7f562fcb14d81fdbc
n-029894 <natzert@roadie.com>
2023-09-26 15:52:01 -0400

    Correct for x86_64 machine types

--- src/scripts/install-dockerize.sh
+++ src/scripts/install-dockerize.sh
@@ -33,7 +33,13 @@ elif cat /etc/issue | grep Alpine &> /dev/null; then
   PLATFORM=alpine-linux-amd64
   apk add --no-cache openssl
 else
-  PLATFORM=linux-$(uname -m)
+  SYS_ARCH=$(uname -m)
+
+  if [[ $SYS_ARCH == "x86_64" ]]; then
+    SYS_ARCH="amd64"
+  fi
+
+  PLATFORM=linux-$SYS_ARCH
 fi
 
 DOCKERIZE_BINARY_URL="https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-$PLATFORM-$DOCKERIZE_VERSION.tar.gz"
